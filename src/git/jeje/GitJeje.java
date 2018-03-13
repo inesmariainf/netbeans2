e20154bec2f0b8a429d51a62f7d9fb752e52b119
inesmariainf <escorreobasura2017@gmail.com>
2018-03-13 09:15:22 +0100

    SUBIDA DEL PROYECTO AL REPOSITORIO 13 marzo 2018 coming 1

--- src/git/jeje/GitJeje.java
+++ src/git/jeje/GitJeje.java
@@ -15,7 +15,7 @@ public class GitJeje {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("ola");
+        System.out.println("hola soy ines");
     }
     
 }
